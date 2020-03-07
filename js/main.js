7d3b353880ac1193ac2cb36106b3f60622451072
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-03-07 21:12:17 +0900

    Добавлен и подключен модуль map

--- js/main.js
+++ js/main.js
@@ -4,96 +4,17 @@
 
 // Необходимые DOM-элементы
 var offersMap = document.querySelector('.map'); // карта с объявлениями
-var pinTemplate = document.querySelector('#pin').content.querySelector('button'); // шаблон метки объявления
-var mapPins = document.querySelector('.map__pins'); // элемент, куда добавлять метки объявлений
 var adForm = document.querySelector('.ad-form'); // форма объявления
 var adFormHeader = document.querySelector('.ad-form-header'); // заголовок формы
 var adFormElements = document.querySelectorAll('.ad-form__element'); // элементы формы
 var mapFilters = document.querySelector('.map__filters'); // форма с фильтрами
 var addressInput = document.querySelector('#address'); // инпут адреса
-var cardTemplate = document.querySelector('#card').content.querySelector('.map__card'); // шаблон карточки объявления
-var mapFiltersContainer = offersMap.querySelector('.map__filters-container'); // контейнер с фильтрами на карте
-
-// Размеры меток
-var pinWidth = 50; // ширина обычной метки
-var pinHeight = 70; // высота обычной метки
 
 var pinMain = {// главная метка
   element: document.querySelector('.map__pin--main'),
   width: 65,
   height: 65,
   pointHeight: 15,
-};
-
-/* ------------------------------ ГЕНЕРАЦИЯ И ОТРИСОВКА МЕТОК ------------------------------ */
-
-// Функция добавления меток во фрагмент и затем на страницу
-var getPins = function () {
-  var pinsFragment = document.createDocumentFragment();
-  for (var i = 0; i < window.data.adverts.length; i++) {
-    var pinElement = pinTemplate.cloneNode(true);
-    offersMap.appendChild(pinElement);
-    pinsFragment.appendChild(window.pin.getPin(window.data.adverts[i], pinElement, pinWidth, pinHeight));
-  }
-  mapPins.appendChild(pinsFragment);
-};
-
-/* ------------------------------ ГЕНЕРАЦИЯ И ОБРАБОТЧИКИ КАРТОЧЕК ------------------------------ */
-
-// Обработчик открытия карточки по клику по метке
-var pinClickHandler = function () {
-  var userPins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
-  userPins.forEach(function (element, i) {
-    element.addEventListener('click', function () {
-      var popup = document.querySelector('.popup');
-      if (popup) {
-        popup.remove();
-      }
-      var cardElement = cardTemplate.cloneNode(true);
-      var cardsFragment = document.createDocumentFragment();
-      cardsFragment.appendChild(window.card.getCard(window.data.adverts[i], cardElement));
-      offersMap.insertBefore(cardsFragment, mapFiltersContainer);
-      closePopup();
-    });
-  });
-};
-
-// Обработчик открытия карточки по нажатию на Enter (на пробел почему-то тоже реагирует)
-var pinEnterPressHandler = function () {
-  var userPins = document.querySelectorAll('.map__pin:not(.map__pin--main)');
-  userPins.forEach(function (element, i) {
-    element.addEventListener('keydown', function (evt) {
-      if (evt.key === 'Enter') {
-        var popup = document.querySelector('.popup');
-        if (popup) {
-          popup.remove();
-        }
-        var cardElement = cardTemplate.cloneNode(true);
-        var cardsFragment = document.createDocumentFragment();
-        cardsFragment.appendChild(window.card.getCard(window.data.adverts[i], cardElement));
-        offersMap.insertBefore(cardsFragment, mapFiltersContainer);
-        closePopup();
-      }
-    });
-  });
-};
-
-// Обработчик закрытия карточки по клику или нажатию на Enter/ESC (почему-то реагирует и на пробел)
-var closePopup = function () {
-  var popup = document.querySelector('.popup');
-  var popupClose = popup.querySelector('.popup__close');
-
-  popupClose.addEventListener('click', function () {
-    popup.remove();
-  });
-
-  popupClose.addEventListener('keydown', function (evt) {
-    window.util.isEnterRemoveEvent(evt, popup);
-  });
-
-  document.addEventListener('keydown', function (evt) {
-    window.util.isEscRemoveEvent(evt, popup);
-  });
 };
 
 /* ------------------------------ ВСПОМОГАТЕЛЬНЫЕ ФУНКЦИИ ДЛЯ СОСТОЯНИЯ СТРАНИЦЫ ------------------------------ */
@@ -130,9 +51,8 @@ addressInput.value = getMainPinCoordinatesValue(pinMain, false);
 var activatePage = function () {
   offersMap.classList.remove('map--faded'); // удаление класса map--faded у карты с объявлениями для ее активации
   adForm.classList.remove('ad-form--disabled'); // удаление класса ad-form--disabled у формы объявления для ее активации
-  getPins(); // вызов функции добавления меток
-  pinClickHandler(); // обработчик по клику по метке
-  pinEnterPressHandler(); // обработчик по нажатию на метку
+  window.map.getPins(); // вызов функции добавления меток
+  window.map.openPopup(); // обработчик по клику по метке
   addressInput.value = getMainPinCoordinatesValue(pinMain, true); // получение координат метки
   adFormHeader.removeAttribute('disabled', 'disabled'); // удаление атрибута disabled с заголовка формы
   mapFilters.removeAttribute('disabled', 'disabled'); // удаление атрибута disabled с формы с фильтрами
