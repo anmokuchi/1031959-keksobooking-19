93e3474bf5f9e829e0b8d6f13c57bb1ad26a6c33
Maria Mikhaleva <maria.mikhaleva@yahoo.com>
2020-03-10 23:18:21 +0900

    Исправлена функция отрисовки метки

--- js/pin.js
+++ js/pin.js
@@ -5,10 +5,15 @@
   var pinTemplate = document.querySelector('#pin').content.querySelector('button'); // шаблон метки объявления
   var mapPins = document.querySelector('.map__pins'); // элемент, куда добавлять метки объявлений
 
+  // Размеры меток
+  var pinWidth = 50; // ширина обычной метки
+  var pinHeight = 70; // высота обычной метки
+
   // Функция отрисовки метки
   var getPin = function (offer, element, width, height, index) {
-    // WH вынести
-    var pinPosition = 'left: ' + (offer.location.x - (width / 2)) + 'px; top: ' + (offer.location.y - height) + 'px;';
+    var left = offer.location.x - (width / 2);
+    var top = offer.location.y - height;
+    var pinPosition = 'left: ' + left + 'px; top: ' + top + 'px;';
 
     element.style = pinPosition;
     element.querySelector('img').src = offer.author.avatar;
@@ -17,10 +22,6 @@
 
     return element;
   };
-
-  // Размеры меток
-  var pinWidth = 50; // ширина обычной метки
-  var pinHeight = 70; // высота обычной метки
 
   // Функция добавления меток во фрагмент и затем на страницу
   var addPins = function () {
